854601436bff2e66433aa6f0366a9af9c81d8620
Sarthak Panigrahi <sipunp70@gmail.com>
2025-03-27 21:08:38 +0530

    binary search in python

---
+++ Assignment2/BinarySearch.py
@@ -0,0 +1,19 @@
+def binary_search(arr,target):
+    left,right=0,len(arr)-1
+    while left <= right:
+        mid = left + (right-left)//2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid+1
+        else:
+            right = mid - 1
+    return -1
+
+arr = [1,2,3,4,5,6,7,8,9]
+target = 8
+result = binary_search(arr, target)
+if result != -1:
+    print(f"Target found at index {result}. ")
+else:
+    print("Target not found.")
